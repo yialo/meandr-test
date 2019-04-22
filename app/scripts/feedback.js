640fb41134c6b1ba730c0a879be0d6f7aea68fc1
Aleksei Arro <polsck@gmail.com>
2019-04-23 01:46:17 +0300

    Form validation added

--- app/scripts/feedback.js
+++ app/scripts/feedback.js
@@ -10,3 +10,40 @@ const telField = feedbackForm
 const aboutField = feedbackForm
   .querySelector('.feedback__field[name=\'about\']');
 
+const onFieldValueMissing = function feedbackFieldValueMissingHandler(field) {
+  const valueMissingMessages = {
+    fullname: 'Пожалуйста, укажите фамалию и имя',
+    email: 'Пожалуйста, введите адрес электронной почты',
+    about: 'Данное поле является обязательным',
+  };
+  const fieldName = field.getAttribute('name');
+  const message = valueMissingMessages[fieldName];
+
+  if (field.validity.valueMissing) {
+    field.setCustomValidity(message);
+  } else {
+    field.setCustomValidity('');
+  }
+};
+
+[fullnameField, emailField, aboutField].forEach((field) => {
+  field.addEventListener('invalid', () => {
+    onFieldValueMissing(field);
+  });
+});
+
+const onTelFieldInvalid = function telFieldInvalidHandler() {
+  const telRegexp = /\+7\s\d{3}\s\d{3}\s\d{4}/;
+
+  if (telField.validity.valueMissing) {
+    telField.setCustomValidity('Пожалуйста, укажите номер телефона');
+  } else if (!telField.value.match(telRegexp)) {
+    telField.setCustomValidity('Введите номер телефона в деятизначном формате: +7 XXX XXX XXXX');
+  } else {
+    telField.setCustomValidity('');
+  }
+};
+
+telField.addEventListener('invalid', () => {
+  onTelFieldInvalid();
+});
