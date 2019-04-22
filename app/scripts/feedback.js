05c490c6ad803eb23240cadc8e6ff8463b6d87e5
Aleksei Arro <polsck@gmail.com>
2019-04-22 23:39:55 +0300

    Validation start

---
+++ app/scripts/feedback.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const feedbackForm = document.querySelector('.feedback__form');
+const fullnameField = feedbackForm
+  .querySelector('.feedback__field[name=\'fullname\']');
+const emailField = feedbackForm
+  .querySelector('.feedback__field[name=\'email\']');
+const telField = feedbackForm
+  .querySelector('.feedback__field[name=\'tel\']');
+const aboutField = feedbackForm
+  .querySelector('.feedback__field[name=\'about\']');
+
